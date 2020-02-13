a4892d77b39486182925cf3224447bf1381a49b4
gusevroman <949397@mail.ru>
2020-02-13 11:45:54 +0300

    merging conflict

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require('hbs');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 
